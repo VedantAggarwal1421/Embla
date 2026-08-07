a21441f4c36efb04a1456a9d564e9a66f43ade7f
Vedant Aggarwal <the.kenshina.1421@gmail.com>
2026-08-07 23:53:27 +0530

    Cleaned up dead code from instruction fetch module and fixed minor bug. Instruction valid bit was never cleared in idle state that could lead to executing same instruction multiple times if fetch was stalled.

--- src/core/IF.sv
+++ src/core/IF.sv
@@ -55,8 +55,14 @@ module instruction_fetch (
             case (if_state)
                 IF_IDLE: begin
                     if (!if_stall) begin
-                        request_pc <= outstanding_pc_4;
-                        if_state   <= IF_REQ;
+                        instruction_valid <= 1'b0;
+                        if (redirect_valid || redirect_req_pending) begin
+                            request_pc <= redirect_valid ? redirect_pc : redirect_req_pc;
+                            redirect_serviced <= 1'b1;
+                        end else request_pc <= outstanding_pc_4;
+                        if_state <= IF_REQ;
+                    end else if (if_id_en) begin
+                        instruction_valid <= 1'b0;
                     end
                 end
                 IF_REQ: begin
@@ -74,20 +80,14 @@ module instruction_fetch (
                 IF_RESP: begin
                     if_req_valid <= 1'b0;
                     if (if_data_valid && !(redirect_req_pending || redirect_valid)) begin  //Response for outstanding pc. Dicard if we need to branch
-                        if (if_stall) begin  //Dont request any more instructions
-                            instruction_valid <= 1'b1;
-                            instruction       <= if_data;
-                            instruction_pc    <= outstanding_pc;
-                            instruction_pc_4  <= outstanding_pc_4;
-                            if_state          <= IF_IDLE;
-                        end else begin  //Everything executing normally
-                            instruction_valid <= 1'b1;
-                            instruction       <= if_data;
-                            instruction_pc    <= outstanding_pc;
-                            instruction_pc_4  <= outstanding_pc_4;
-                            if_state          <= IF_REQ;
-                            request_pc        <= outstanding_pc_4;
-                        end
+
+                        instruction_valid <= 1'b1;
+                        instruction       <= if_data;
+                        instruction_pc    <= outstanding_pc;
+                        instruction_pc_4  <= outstanding_pc_4;
+                        request_pc        <= outstanding_pc_4;
+                        if_state          <= (if_stall) ? IF_IDLE : IF_REQ;
+
                     end else if (if_data_valid) begin  //Redirect active
                         if_state          <= IF_REQ;
                         request_pc        <= (redirect_valid) ? redirect_pc : redirect_req_pc;
@@ -101,53 +101,3 @@ module instruction_fetch (
         end
     end
 endmodule
-
-/*
-    logic [31:0] pc;
-    logic [31:0] old_pc;
-
-    logic [31:0] fetch_buff_instr;
-    logic [31:0] fetch_buff_pc;
-    logic [31:0] fetch_buff_pc_4;
-    logic        fetch_buff_valid;
-
-
-    assign if_req_valid = ~if_stall;  //If not stalled request instructions
-    assign if_addr = pc;
-
-    always_ff @(posedge clk or posedge rst) begin
-        if (rst) begin
-            pc <= 32'd0;
-            old_pc <= 32'd0;
-        end else if (if_req_valid) begin
-            if (redirect_valid) begin
-                pc <= redirect_pc;
-                old_pc <= pc;
-            end else begin
-                pc <= pc + 32'd4;
-                old_pc <= pc;
-            end
-        end
-    end
-
-    always_ff @(posedge clk or posedge rst) begin
-        if (rst) begin
-            fetch_buff_instr <= 32'b0;
-            fetch_buff_pc <= 32'b0;
-            fetch_buff_pc_4 <= 32'b0;
-            fetch_buff_valid <= 1'b0;
-        end else if (!if_req_valid && if_data_valid) begin
-            fetch_buff_instr <= if_data;
-            fetch_buff_pc <= old_pc;
-            fetch_buff_pc_4 <= pc;
-            fetch_buff_valid <= 1'b1;
-        end else if (fetch_buff_valid && if_id_en) begin
-            fetch_buff_valid <= 1'b0;
-        end
-    end
-
-    assign instruction_valid = (fetch_buff_valid) ? 1'b1 : if_data_valid;
-    assign instruction = (fetch_buff_valid) ? fetch_buff_instr : (if_data_valid) ? if_data : 32'b0;
-    assign instruction_pc = (fetch_buff_valid) ? fetch_buff_pc : old_pc;
-    assign instruction_pc_4 = (fetch_buff_valid) ? fetch_buff_pc_4 : pc;
-    */
